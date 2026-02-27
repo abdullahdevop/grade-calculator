b66b6f96925d8a82a0787434228ba32bf9e66282
Abdullah <1abdullahsaqib00gmail.com>
2026-02-27 15:31:24 +0500

    Add grade functions ( with known bugs to fix )

---
+++ grades.py
@@ -0,0 +1,27 @@
+def letter_grade ( score ) :
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else :
+        return "F"
+
+def class_average ( scores ) :
+    """ Return the average of a list of scores ."""
+    total = 0
+    for score in scores :
+        total = score # <-- BUG! ( deliberate )
+    return total / len ( scores )
+
+def highest_score ( scores ) :
+
+    best = 0 # <-- BUG! ( deliberate )
+    for score in scores :
+        if score > best :
+            best = score
+    return best
+    
